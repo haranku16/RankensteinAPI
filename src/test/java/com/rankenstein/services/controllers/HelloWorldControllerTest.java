33bb934a5d72fd222628e25ee4a5f0c43a5aaee6
Harankumar Nallasivan <haranku16@gmail.com>
2018-05-31 00:17:03 -0700

    Fixing test

--- src/test/java/com/rankenstein/services/controllers/HelloWorldControllerTest.java
+++ src/test/java/com/rankenstein/services/controllers/HelloWorldControllerTest.java
@@ -1,12 +1,31 @@
 package com.rankenstein.services.controllers;
 
+import com.rankenstein.services.response.Response;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class HelloWorldControllerTest {
+public class HelloWorldControllerTest extends HelloWorldController {
+
+    private Response<String> response;
+
+    @Before
+    public void initialize() {
+        response = greet("world");
+    }
+
     @Test
-    public void test() {
-        assertEquals("Hello, world!", new HelloWorldController().greet("world"));
+    public void testData() {
+        assertEquals("Hello, world!", response.getData());
+    }
+
+    @Test
+    public void testMetadata() {
+        Response.Metadata metadata = response.getMetadata();
+        assertEquals(1, metadata.getPage());
+        assertEquals(1, metadata.getSeed());
+        assertEquals(1, metadata.getSize());
+        assertEquals(200, metadata.getStatus());
     }
 }
